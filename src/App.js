e37918973a39a1fc2b22f38efe50f4bfc0b04aa7
mostafa93sh <mostafa.khalel.93@gmail.com>
2023-05-02 22:02:31 +0200

    adding calendar page

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 // import Invoices from "./scenes/invoices";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
@@ -33,11 +33,11 @@ function App() {
               <Route path='/team' element={<Team />} />
               <Route path='/contacts' element={<Contacts />} />
               <Route path='/form' element={<Form />} />
+              <Route path='/bar' element={<Bar />} />
+              <Route path='/faq' element={<FAQ />} />
+              <Route path='/pie' element={<Pie />} />
               {/* <Route path="/invoices" element={<Invoices />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/geography" element={<Geography />} /> */}
             </Routes>
